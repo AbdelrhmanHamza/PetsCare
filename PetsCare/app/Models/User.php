db3ab1fdedb825367bdfdfb95e534360af57f82b
AmrSamy36 <amrabm36@gmail.com>
2022-03-28 23:28:03 +0200

    edit user model with soft delete

--- PetsCare/app/Models/User.php
+++ PetsCare/app/Models/User.php
@@ -5,12 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Laravel\Sanctum\HasApiTokens;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 
 class User extends Authenticatable implements JWTSubject
 {
     use HasApiTokens, HasFactory, Notifiable;
+    use SoftDeletes;
+
 
     /**
      * The attributes that are mass assignable.
@@ -33,6 +36,8 @@ class User extends Authenticatable implements JWTSubject
         'password',
         'remember_token',
     ];
+    protected $dates = ['deleted_at'];
+
 
     /**
      * The attributes that should be cast.
